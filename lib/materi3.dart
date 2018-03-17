4b3750920a7db505fe32f14f936382626a933080
omrobbie <putu.roby@yahoo.com>
2018-03-17 16:15:51 +0800

    materi3

---
+++ lib/materi3.dart
@@ -0,0 +1,11 @@
+main() {
+  var nilai = 10;
+
+  if (nilai > 50) {
+    print('Anda lulus');
+  } else if (nilai < 40) {
+    print('Nilai anda terlalu rendah');
+  } else {
+    print('Anda gagal');
+  }
+}
