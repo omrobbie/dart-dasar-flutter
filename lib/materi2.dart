6b9fa4ea74db2635719fa562e53e58116512c939
omrobbie <putu.roby@yahoo.com>
2018-03-17 16:12:25 +0800

    materi2

---
+++ lib/materi2.dart
@@ -0,0 +1,11 @@
+main() {
+  for (int i = 0; i < 100; i++) {
+    print('Nilai i = $i');
+  }
+
+  var nama = 'omrobbie';
+
+  for (int x = 0; x < nama.length; x++) {
+    print('Huruf ke $x adalah ${nama[x]}');
+  }
+}
